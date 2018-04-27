7ee93a0eb1ddebb4dd501adebc45f94924e20deb
Thaya Nathan <thaya.nathan@orionhealth.com>
2018-04-27 22:46:52 +1200

    Initial commit

---
+++ scripts/configure-timezone.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -e
+
+timezone=${TIME_ZONE:-America/Los_Angeles}
+
+echo "### Setting timezone to $timezone ###"
+
+sudo tee /etc/sysconfig/clock << EOF > /dev/null
+ZONE= "$timezone"
+UTC=true
+EOF
+
+sudo ln -sf /usr/share/zoneinfo/$timezone /etc/localtime
+
+sudo chkconfig ntpd on
